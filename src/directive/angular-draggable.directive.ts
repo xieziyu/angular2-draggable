bbefd3b2143a89ae9bec5c7e2fb3ea923e6942bf
Tyler Lindell <tlindell@tesla.com>
2017-05-04 23:46:48 +0100

    adding z-index to element being dragged and removing when finished dragging

--- src/directive/angular-draggable.directive.ts
+++ src/directive/angular-draggable.directive.ts
@@ -34,7 +34,7 @@ export class AngularDraggableDirective implements OnInit {
 
   ngOnInit() {
     this.renderer.setElementStyle(this.el.nativeElement, 'position', 'relative');
-    
+
     if (this.allowDrag) {
       
       let element = this.handle ? this.handle : this.el.nativeElement;
@@ -71,6 +71,7 @@ export class AngularDraggableDirective implements OnInit {
 
   @HostListener('document:mouseup')
   onMouseUp() {
+    this.el.nativeElement.style.removeProperty('z-index');
     this.moving = false;
   }
 
@@ -82,6 +83,7 @@ export class AngularDraggableDirective implements OnInit {
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event:MouseEvent) {
     if (this.moving && this.allowDrag) {
+      this.el.nativeElement.style.zIndex = "99999";
       this.moveTo(event.clientX, event.clientY);
     }
   }

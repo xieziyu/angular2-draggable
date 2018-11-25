5d33c9239354058af27286a8be7bde0cf1575935
markoj21 <markoj21@gmail.com>
2018-11-25 14:38:44 -0800

    Update public_api.ts
    Add the missing types for size and resize events.

--- projects/angular2-draggable/src/public_api.ts
+++ projects/angular2-draggable/src/public_api.ts
@@ -6,3 +6,6 @@ export * from './lib/angular-draggable.directive';
 export * from './lib/angular-resizable.directive';
 export * from './lib/angular-draggable.module';
 export * from './lib/models/position';
+export * from './lib/models/size.d';
+export * from './lib/models/resize-event.d.ts';
+export * from './lib/models/resize-handle-type.d';
